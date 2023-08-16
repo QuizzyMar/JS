57ca9e0f633108c434ddc51337f93530cb9d0680
Bulavka <bulavka081993@gmail.com>
2023-08-16 16:02:54 +0300

    add let and console for jsHW_L1.js

---
+++ Lesson1/jsHW_L1.js
@@ -0,0 +1,43 @@
+let s1='hello';
+console.log(s1);
+let s2='owu';
+console.log(s2);
+let s3='com';
+console.log(s3);
+let s4='ua';
+console.log(s4);
+
+let n1=1;
+console.log(n1);
+let n2=10;
+console.log(n2);
+let n3=-999;
+console.log(n3);
+let n4=123;
+console.log(n4);
+let n5=3.14;
+console.log(n5);
+let n6=2.7;
+console.log(n6);
+let n7=16;
+console.log(n7);
+
+let b1=30;
+let b2=20;
+let bRes=b1>b2;
+console.log(bRes);
+console.log(!!0);
+
+let firstName='Maryna ';
+let middleName='Mukolaivna ';
+let lastName='Bulavka ';
+let person= firstName+middleName+lastName;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
