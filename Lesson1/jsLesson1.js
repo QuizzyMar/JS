9a7005cfd4821f98b9085412c64deddfb91438cc
Bulavka <bulavka081993@gmail.com>
2023-08-16 13:03:12 +0300

    add  Lesson1.html add  jsLesson1.js

---
+++ Lesson1/jsLesson1.js
@@ -0,0 +1,16 @@
+// let a1;
+// a1 = 'hello Maryna';
+
+let a1='hello Maryna'   //''-string/
+console.log(a1);
+
+let b1 = 123;      // number 123, без лапок!
+console.log(b1);
+b1=123;
+
+const c1='bla';
+console.log(c1);
+// c1 = 1111;
+
+var v1='blabla';
+console.log(v1);
